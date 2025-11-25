b0e4835e6a9a70253455900f2efc5539d969269f
paras027 <parasbhardwaj.ps@gmail.com>
2025-11-25 12:20:29 +0530

    fixed cors

--- src/app.js
+++ src/app.js
@@ -9,10 +9,14 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: ['http://localhost:5173','http://localhost:5174','https://courtline.netlify.app'], // your frontend URL
-    credentials: true,               // allow cookies to be sent
+    origin: ['http://localhost:5173', 'http://localhost:5174', 'https://courtline.netlify.app'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
   })
 );
+
+
 app.use(bodyParser.json());
 app.use(express.json());
 
